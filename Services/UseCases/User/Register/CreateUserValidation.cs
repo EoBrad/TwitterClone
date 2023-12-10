9b51a5a7326d4d68e2bf8cea08425fc827e3d9f7
João Pontes <joao.alencar099@gmail.com>
2023-12-10 15:25:10 -0300

    Configure Validation for CreateUser request

---
+++ Services/UseCases/User/Register/CreateUserValidation.cs
@@ -0,0 +1,37 @@
+using FluentValidation;
+using TwitterClone.Dtos;
+
+namespace TwitterClone.Services.UseCases.User.Register;
+
+public class CreateUserValidation : AbstractValidator<CreateUserDto>
+{
+    public CreateUserValidation()
+    {
+        RuleFor(x => x.Username).NotEmpty()
+                                .MinimumLength(5)
+                                .MaximumLength(35);
+
+        RuleFor(x => x.Password).NotEmpty()
+                                .MinimumLength(5)
+                                .MaximumLength(20);
+
+        RuleFor(x => x.Name).NotEmpty()
+                            .MinimumLength(3)
+                            .MaximumLength(20);
+
+        RuleFor(x => x.Email).NotEmpty()
+                            .EmailAddress();
+
+
+        RuleFor(x => x.LastName).NotEmpty()
+                                .MinimumLength(3);
+
+        RuleFor(x => x.DateOfBirth).NotEmpty().Custom((date, context) => 
+        {
+            if (date.Year - DateTime.Now.Year < 18)
+            {
+                context.AddFailure("DateOfBirth", "User must be at least 18 years old");
+            }
+        });
+    }
+}
