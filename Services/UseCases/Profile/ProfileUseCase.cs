21ded2e8f64d4ceb62184bca680edad2c26af5e3
João Pontes <joao.alencar099@gmail.com>
2023-12-12 09:51:02 -0300

    Add ProfileUseCase

---
+++ Services/UseCases/Profile/ProfileUseCase.cs
@@ -0,0 +1,38 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+using AutoMapper;
+using TwitterClone.Exeptions;
+using TwitterClone.Models;
+using TwitterClone.Responses;
+
+namespace TwitterClone.Services.UseCases.Profile;
+
+public class ProfileUseCase : IProfileUseCase
+{
+    private readonly IUserRepository _userRepository;
+
+    private readonly IMapper _mapper;
+
+
+    public ProfileUseCase(IUserRepository userRepository, IMapper mapper)
+    {
+        _userRepository = userRepository;
+        _mapper = mapper;
+    }
+ 
+    public async Task<UserProfileResponse> Execute(string username)
+    {
+        if (username.Contains('.'))
+            throw new TwitterCloneExeption("Invalid username", (int)HttpStatusCode.BadRequest);
+
+        var user = await _userRepository.FindUserByEmailOrUsername(username);
+
+        if (user == null)
+            throw new TwitterCloneExeption("User not found", (int)HttpStatusCode.NotFound);
+
+        return _mapper.Map<UserProfileResponse>(user);
+    }
+}
